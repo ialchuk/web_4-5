8889ddd3ea06eed2f8382eb6941e426315293f6f
Ivan Alchuk <ivan.a@nerdysoft.com>
2021-01-19 14:18:42 +0200

    changes for deploy

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./redux/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Provider store={store}>
               <App/>
           </Provider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
